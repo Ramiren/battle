c16f6f32845d8d778b2f4ea8045959bfdceb3c98
Ramiren <tobiashendry@gmail.com>
2017-11-29 07:58:58 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -7,24 +7,28 @@ require_relative 'battle'
 
 #Ausführung 
 def start
+ 
 puts "Willkommen zu Battle of Students -"
 puts "Zwei Studenten kämpfen mit ihren selbsgewählten Waffen gegeneinander!"
+puts
+puts 'Das Wetter hat einen Einfluss auf den Kampf'
+Studenten.auswirkung(Wetter.wieistwetter)
+puts Wetter.wieistwetter.status
+sleep(1)
 puts
  
 #zeigt alle Studenten (Kämpfer) an
   Studenten.alle
   puts
- 
+
 #ruft Funktion battle auf mit Parameter = Objekten aus Inputs  
 battle(kämpfer1_auswahl, kämpfer2_auswahl, waffenwahl1, waffenwahl2)
 
 end
 
-
 #main wiederholen
 def wiederholen
-  puts
-  puts
+  10.times {puts}
   print "Weiter? 1 = Ja, 2 = Nein : "
   eingabe = gets.chomp
   
@@ -39,8 +43,6 @@ def wiederholen
 end
 
 start
-
-
 
 
 
